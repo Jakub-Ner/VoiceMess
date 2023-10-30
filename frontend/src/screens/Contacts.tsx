42c053d363c9e9aaebc30caf00ab0db99507cb1c
jakub-ner <kubaner1@gmail.com>
2023-10-30 22:54:17 +0100

    pretify contacts

--- frontend/src/screens/Contacts.tsx
+++ frontend/src/screens/Contacts.tsx
@@ -1,59 +1,74 @@
 import React from 'react';
-import {Button, ButtonGroup, Input, Layout, List, ListItem} from '@ui-kitten/components';
-import {StyleSheet} from "react-native";
-import {StatusBar} from "expo-status-bar";
+import { Button, ButtonGroup, Input, Layout, List, ListItem } from '@ui-kitten/components';
+import { Dimensions, Image, StyleSheet } from "react-native";
 
 interface IListItem {
-    title: string;
-    description: string;
+  title: string;
+  description: string;
 }
 
 export default function Contacts() {
-    const [value, setValue] = React.useState('');
+  const [value, setValue] = React.useState('');
 
-    const renderItemAccessory = (): React.ReactElement => (
-        <ButtonGroup>
-            <Button size='tiny'>
-                ▷
-            </Button>
-            <Button size='tiny'>
-                ↻
-            </Button>
-        </ButtonGroup>
+  const renderItemAccessory = (): React.ReactElement => (
+    <ButtonGroup>
+      <Button size='tiny'>
+        ▷
+      </Button>
+      <Button size='tiny'>
+        ↻
+      </Button>
+    </ButtonGroup>
 
-    );
+  );
 
-    const renderItem = ({item, index}: { item: IListItem; index: number }): React.ReactElement => (
-        <ListItem
-            title={`${item.title} ${index + 1}`}
-            description={`${item.description} ${index + 1}`}
-            accessoryRight={renderItemAccessory}
-        />
-    );
+  const renderItem = ({item, index}: { item: IListItem; index: number }): React.ReactElement => (
+    <ListItem
+      title={`${item.title} ${index + 1}`}
+      description={`${item.description} ${index + 1}`}
+      accessoryRight={renderItemAccessory}
+    />
+  );
 
-    return (
-        <Layout style={styles.container}>
-            <Input
-                placeholder='🔍  Szukaj'
-                value={value}
-                onChangeText={nextValue => setValue(nextValue)}
-            />
-            <List
-                style={styles.container}
-                data={data}
-                renderItem={renderItem}
-            />
-        </Layout>
-    );
+  return (
+    <Layout style={styles.container}>
+      <Image source={require('../../assets/logo.png')} style={styles.image}/>
+      <Input
+        style={styles.input}
+        placeholder='🔍  Szukaj'
+        value={value}
+        onChangeText={nextValue => setValue(nextValue)}
+      />
+      <List
+        style={styles.container}
+        data={data}
+        renderItem={renderItem}
+      />
+    </Layout>
+  );
 }
 
 const data = new Array(13).fill({
-    title: 'Osoba nr',
-    description: 'Wiadomosc nr ',
+  title: 'Osoba nr',
+  description: 'Wiadomosc nr ',
 });
 
 const styles = StyleSheet.create({
-    container: {
-        height: '100%',
-    },
+  container: {
+    marginTop: Dimensions.get('window').height * 0.04,
+    flex: 1,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    margin: 20,
+    justifyContent: 'center',
+    alignSelf: 'center',
+  },
+  input: {
+    width: '70%',
+    alignSelf: 'center',
+    borderRadius: 50,
+  }
 });
