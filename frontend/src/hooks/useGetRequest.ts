7bed687278ce4b594f84c146fa31528aee7908e1
jakub-ner <kubaner1@gmail.com>
2023-11-02 21:07:31 +0100

    fix ip

--- frontend/src/hooks/useGetRequest.ts
+++ frontend/src/hooks/useGetRequest.ts
@@ -1,5 +1,5 @@
 
-export default function useGetRequest(url = 'http://127.0.0.1:8080/') {
+export default function useGetRequest(url = 'http://192.168.14.225:8080') {
     fetch(url)
         .then(response => response.json())
         .then(data => console.log(data))
