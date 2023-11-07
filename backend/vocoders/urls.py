1278545f8d6d520b23051cf14bc4c47281505b33
jakub-ner <kubaner1@gmail.com>
2023-11-07 12:20:42 +0100

    list vocoders for a given customer

--- backend/vocoders/urls.py
+++ backend/vocoders/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import VocoderViewSet
 
 urlpatterns = [
-    path("", VocoderViewSet.as_view({"get": "list", "post": "create"})),
+    path("", VocoderViewSet.as_view({"post": "create"})),
+    path("<str:customer_id>/", VocoderViewSet.as_view({"get": "list"})),
     path("<int:pk>/", VocoderViewSet.as_view({"get": "retrieve", "put": "update", "delete": "destroy"})),
 ]
--- backend/vocoders/views.py
+++ backend/vocoders/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.response import Response
+
 # Create your views here.
 
 from .serializers import VocoderSerializer
 from .models import Vocoder
+from customer.models import Customer
 
 
 class VocoderViewSet(viewsets.ModelViewSet):
@@ -12,4 +14,11 @@ class VocoderViewSet(viewsets.ModelViewSet):
     """
     serializer_class = VocoderSerializer
     queryset = Vocoder.objects.all()
+
     # permission_classes = [permissions.IsAuthenticated]
+
+    def list(self, request, *args, **kwargs):
+        customer_id = kwargs['customer_id']
+        customer = Customer.objects.filter(pk=customer_id).first()
+        vocoder_list = Vocoder.objects.filter(customer=customer)
+        return Response(self.serializer_class(vocoder_list, many=True).data)
