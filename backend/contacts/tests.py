bfd32994e5cbdf1f2fb0a7c797fa1001584560fa
agata <agabela121@gmail.com>
2024-01-23 21:19:34 +0100

    contacts & customer tests

--- backend/contacts/tests.py
+++ backend/contacts/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from customer.models import Customer
+from vocoders.models import Vocoder
+from contacts.models import Contact
+from django.urls import reverse
+class ContactCreateTestCase(TestCase):
+    def setUp(self):
+        self.customer = Customer.objects.create(facebook_id="Test Customer", card_id="123456789")
+        self.vocoder = Vocoder.objects.create(eleven_labs_id="123", name="Test Vocoder", customer_id=self.customer)
 
-# Create your tests here.
+    def test_add_vocoder_id_to_contact(self):
+        contact = Contact.objects.create(contact_id="123", customer_id=self.customer, vocoder_id=self.vocoder)
+
+        saved_contact = Contact.objects.get(contact_id="123")
+
+        self.assertEqual(saved_contact.vocoder_id, self.vocoder)
+        
--- backend/customer/tests.py
+++ backend/customer/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from django.urls import reverse
+from .models import Customer
+from .serializers import CustomerSerializer
 
-# Create your tests here.
+class CustomerCreateTestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.customer = Customer.objects.create(facebook_id="Test Customer", card_id="123456789",)
+
+    def test_get_all_customers(self):
+        url = reverse('customer-list')
+
+        response = self.client.get(url)
+
+        customers = Customer.objects.all()
+        serializer = CustomerSerializer(customers, many=True)
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(response.data, serializer.data)
+        
+    def test_create_customer_with_invalid_data(self):
+        url = reverse('customer-list')
+
+        data = {
+            'facebook_id': '',
+            'card_id': '123456789',
+        }
+
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, 400)
+
+        self.assertEqual(Customer.objects.count(), 1)
